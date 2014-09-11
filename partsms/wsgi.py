cef3a78b01deb2e2e7ad8a9312a6f60b3ac8245a
jcchen <junchangchen@gmail.com>
2014-09-11 18:36:08 +0800

    wsgi prod

--- partsms/wsgi.py
+++ partsms/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "partsms.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "partsms.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
