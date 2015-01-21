1987afe219ebb8d0a69cde7713f7b39c29e5855d
jcchen <junchangchen@gmail.com>
2015-01-21 17:05:20 +0800

    add js

---
+++ partsrequest/static/partsrequest/js/parts_request.js
@@ -0,0 +1,11 @@
+
+jQuery(function($) {
+    
+    $("#id_employee_0").on("djselectableselect", function(e) {
+        console.log( '领用人改了啊...' );
+    });
+});
+
+function auto_complete_employee_num_and_department() {
+    
+}
